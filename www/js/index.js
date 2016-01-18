d25fef488d8bcb10a55a1314f572f6ad6453e086
Marc Majcher <majcher@gmail.com>
2016-01-18 05:16:18 -0600

    getting there

--- www/js/index.js
+++ www/js/index.js
@@ -2,10 +2,9 @@
 function init() {
     $('#btnserver').hide();
 
-    var zc;
     $('#btnadd').click(function(){
 
-        zc = cordova.plugins.zeroconf;
+        var zc = cordova.plugins.zeroconf;
         zc.register('_http._tcp.local.', 'DynoForce-'+device.model+'-'+device.uuid, 80, { 'id': 'DynoForce'});
         zc.watch('_http._tcp.local', function(result) {
             var action = result.action;
@@ -25,10 +24,45 @@ function init() {
         $('#btnserver').show();
     });
 
-    // $('#btnremove').click(function() {
-    //     zc.stop();
-    //     console.log("unregistered")
-    // });
+    $('#btntest').click(function(){
+        alert('test');
+    });
+
+    $('#btnserver').click(function() {
+        alert('starting server')
+        var wsserver = cordova.plugins.wsserver;
+
+
+        // wsserver.start(port, {
+        //     'onStart' : function(addr, port) {
+        //         console.log('Listening on %s:%d', addr, port);
+        //     },
+        //     'onStop' : function(addr, port) {
+        //         console.log('Stopped listening on %s:%d', addr, port);
+        //     },
+        //     'onOpen' : function(conn) {
+        //          conn: {
+        //          'uuid' : '8e176b14-a1af-70a7-3e3d-8b341977a16e',
+        //          'remoteAddr' : '192.168.1.10',
+        //          'acceptedProtocol' : 'my-protocol-v1',
+        //          'httpFields' : {...}
+        //          } 
+        //         console.log('A user connected from %s', conn.remoteAddr);
+        //     },
+        //     'onMessage' : function(conn, msg) {
+        //         console.log(conn, msg);
+        //     },
+        //     'onClose' : function(conn) {
+        //         console.log('A user disconnected from %s', conn.remoteAddr);
+        //     },
+        //     'origins' : [ 'file://' ] // optional. validates the 'Origin' HTTP Header. 
+        //     'protocols' : [ 'my-protocol-v1', 'my-protocol-v2' ] // optional. validates the 'Sec-WebSocket-Protocol' HTTP Header. 
+        // });
+
+        alert('server ok')
+    });
+
+
 }
 
 $(document).ready(init);
