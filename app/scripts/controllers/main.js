51516fa14030fe10e320fff1b9abbcaa30fef9f7
Marc Majcher <majcher@gmail.com>
2016-01-19 16:18:06 -0600

    initial angular/grunt loadin

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dynoforceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dynoforceApp
+ */
+angular.module('dynoforceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
